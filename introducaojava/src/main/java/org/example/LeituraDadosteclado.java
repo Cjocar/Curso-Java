8a73704849dbe1717e8412ca6d362d0895b7f483
Cjocar <167594403+Cjocar@users.noreply.github.com>
2024-09-10 22:15:11 -0300

    melhoria no codigo

--- introducaojava/src/main/java/org/example/LeituraDadosteclado.java
+++ introducaojava/src/main/java/org/example/LeituraDadosteclado.java
@@ -7,7 +7,7 @@ public class LeituraDadosteclado {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite seu nome completo:");
+        /*System.out.println("Digite seu nome completo:");
         String nomeCompleto = scan.nextLine();
         System.out.println("Seu nome completo é: " + nomeCompleto);
 
@@ -19,6 +19,27 @@ public class LeituraDadosteclado {
         int idade = scan.nextInt();
         System.out.println("Sua idade é: " + idade);
 
-        System.out.println("Seu nome completo é: " + nomeCompleto + "\nSeu primeiro nome é: " + primeiroNome + "\nSua idade é: " + idade);
+        System.out.println("Digite a sua altura: ");
+        double altura = scan.nextDouble();
+        System.out.println("A sua altura é: " + altura);*/
+
+        System.out.println("Digite o primeiro nome, idade, quantidade de filhos, altura, se tem bixinhos de estimacao");
+        String primeiroNome = scan.next();
+        int idade = scan.nextInt();
+        byte qntFilhos = scan.nextByte();
+        float altura = scan.nextFloat();
+        boolean temPet =scan.nextBoolean();
+
+        System.out.println("Voce digitou os seguintes valores");
+        System.out.println("Primeiro nome: " + primeiroNome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Quantidade de Filhos: " + qntFilhos);
+        System.out.println("Altura: " + altura);
+        System.out.println("Tem bixinhos de estimação " + temPet);
+
+
+
+
+        //System.out.println("Seu nome completo é: " + nomeCompleto + "\nSeu primeiro nome é: " + primeiroNome + "\nSua idade é: " + idade);
     }
 }
